979afd335f7b498962a92f00a9fb902fe92117dc
ysevi <ysevill2@calstatela.edu>
2026-05-02 13:25:04 -0700

    created config file to load feature parameters to. updated Feature notebook, will rename

--- Functions/feature_correlation.py
+++ Functions/feature_correlation.py
@@ -6,27 +6,8 @@ import pandas as pd
 from sklearn.datasets import fetch_openml
 from sklearn import metrics
 from sklearn.preprocessing import StandardScaler
-from yaml import safe_load
-
-# Path setup
-data_dir = "../Data"
-third_party_dir = "../ThirdParty"
-output_dir = "../Out"
-file_path = os.path.join(data_dir, "spotify_dataset.csv")
-
-# Load the latest version
-df = pd.read_csv(file_path)
-
-# TODO: create yaml and populate with these config
-features = [
-    "danceability", "energy", "loudness", "speechiness",
-    "acousticness", "instrumentalness", "liveness",
-    "valence", "tempo", "duration_ms", "popularity",
-    "mode", "time_signature"
-]
 
 def feature_scaling(X):
-  X["explicit"] = df["explicit"].astype(int)
   scaler = StandardScaler()
   X_scaled = scaler.fit_transform(X)
   return X_scaled
@@ -57,5 +38,3 @@ def inv_cov_matrix(X):
   cov = np.cov(X_scaled, rowvar=False)
   VI = np.linalg.pinv(cov)
   return VI
-
-feature_correlation(df[features].copy())
